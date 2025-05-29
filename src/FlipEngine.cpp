83080f147c63fc09d4e1e801ad3afa78104ee133
brighht <bright.andohh@gmail.com>
2025-05-29 18:02:37 -0500

    FlipEngine Test flight

--- src/FlipEngine.cpp
+++ src/FlipEngine.cpp
@@ -8,3 +8,39 @@ FlipEngine::FlipEngine()
       {
         obstacles.emplace_back(sf::Vector2f(100.f,50.f), sf::Vector2f(600.f,50.f));
       }
+
+void FlipEngine::run(){
+    while(gameWindow.isOpen()){
+        deltaTime = dtClock.restart().asSeconds();
+        
+        processInput();
+        updateGame();
+        render();
+    }
+}
+
+void FlipEngine::processInput(){
+    sf::Event userActivity;
+    while(gameWindow.pollEvent(userActivity)){
+        if(userActivity.type == sf::Event::Closed){
+            gameWindow.close();
+        }
+    }
+
+    player.handleInput();
+}
+
+void FlipEngine::updateGame(){
+    player.update(deltaTime,obstacles);
+
+}
+
+void FlipEngine::render(){
+    gameWindow.clear();
+
+    for(auto &obs: obstacles){
+        obs.draw(gameWindow);
+    }
+    player.draw(gameWindow);
+    gameWindow.display();
+}
